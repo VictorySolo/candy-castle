321cfc4389f20fa3fef139ebad43c9c2263fe4ae
mflopp <megaflopp@gmail.com>
2024-10-28 19:56:49 +0200

    customer router created

--- backend/routes/customersRouter.js
+++ backend/routes/customersRouter.js
@@ -1,6 +1,20 @@
-const express = require('express');
+// -- importing modules
+const express = require("express");
 const router = express.Router();
+// -- importing functions from customersController
+const {
+  allOrders,
+  gettingById,
+  gettingAll,
+  updating,
+  deleting,
+  creating,
+} = require("../controllers/customersController");
 
-// Define your routes here
-
+// -- setting up routes for customers
+router.route("/").get(gettingAll).post(creating);
+router.route("/:id").get(gettingById).put(updating).delete(deleting);
+// -- setting up route for getting all orders
+router.get("/:id/allorders", allOrders);
+// -- exporting routes
 module.exports = router;
