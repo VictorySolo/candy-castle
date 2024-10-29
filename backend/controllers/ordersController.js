f6ccde5e01ce5c4868a050e88708cead9a77df29
mflopp <megaflopp@gmail.com>
2024-10-29 18:47:00 +0200

    orderController fixed

--- backend/controllers/ordersController.js
+++ backend/controllers/ordersController.js
@@ -2,17 +2,37 @@
 const Order = require("../models/order");
 const Customer = require("../models/customer");
 const Cart = require("../models/cart");
+const Product = require("../models/product");
 
 // -- creating a new order for a customer
-const createNewOrder = async (req, res) => {
+const createNewOrder = async (req, res, next) => {
   try {
     // -- getting parameters from the request
     const { customerId, deliveryAddress } = req.body;
+    // -- checking if all necessary parameters exist
+    if (!customerId || !deliveryAddress) {
+      return res.status(400).json({ message: "Not enough parameters" });
+    }
     // -- looking for the customer's cart
     const cart = await Cart.findOne({ customerId }).populate("items.productId");
     // -- if the cart doesn't exist send a message to client
     if (!cart || cart.items.length === 0) {
       return res.status(400).json({ message: "Cart is empty" });
+    }
+
+    // -- checking if the amounts of products in the cart are available and updating them
+    for (const item of cart.items) {
+      const product = await Product.findById(item.productId);
+      if (item.amount > product.amount) {
+        return res
+          .status(400)
+          .json({ message: `Not enough amount of ${product.name}` });
+      }
+      product.amount -= item.amount;
+      if (product.amount === 0) {
+        product.availability = false;
+      }
+      await product.save();
     }
 
     // -- calculating total price of the order
@@ -27,6 +47,7 @@ const createNewOrder = async (req, res) => {
       })),
       deliveryAddress,
       price: totalPrice,
+      customer: customerId,
     });
 
     // -- adding the new order to the customer's order list
@@ -41,8 +62,21 @@ const createNewOrder = async (req, res) => {
     // -- clearing the customer's cart
     cart.items = [];
     await cart.save();
+    // -- formatting the response
+    const responseOrder = {
+      date: newOrder.date,
+      products: newOrder.products.map((item) => ({
+        id: item.product._id,
+        name: item.product.name,
+        amount: item.amount,
+        price: item.product.price,
+      })),
+      deliveryAddress: newOrder.deliveryAddress,
+      price: newOrder.price,
+      customer: newOrder.customer,
+    };
     // -- Order created response to the client
-    res.status(201).json({ message: "Order created", order: savedOrder });
+    res.status(201).json({ message: "Order created", order: responseOrder });
   } catch (error) {
     console.log("Error creating order");
     // -- handling the error
@@ -54,7 +88,7 @@ const createNewOrder = async (req, res) => {
 const cancelOrder = async (req, res) => {
   try {
     // -- getting orderId from request
-    const orderId = req.params.id
+    const orderId = req.params.id;
     // -- looking for the order by ID
     const order = await Order.findById(orderId);
     // -- if order doesn't exist send a message to client
