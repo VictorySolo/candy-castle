00e00600c14407fb43d50745cb793dce6fe984b4
VictorySolo <victorysolovyova@gmail.com>
2024-10-29 18:06:15 +0200

    created products routes

--- backend/routes/productsRouter.js
+++ backend/routes/productsRouter.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const {
+    gettingAll,
+    createProduct,
+    getProductById,
+    updateProduct,
+    deleteProductById,
+    allProductsByCategory,
+    allReviews
+} = require('../controllers/productController');
 
-// Define your routes here
+router.route("/").get(gettingAll).post(createProduct)
+router.route("/:id").get(getProductById).put(updateProduct).delete(deleteProductById)
+router.route("/category/:name").get(allProductsByCategory)
+// router.route("/reviews/:id").get(allReviews)
 
 module.exports = router;
