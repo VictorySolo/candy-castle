3cc849111c323879f21f74bfc6aa23889302756f
mflopp <megaflopp@gmail.com>
2024-11-04 04:37:08 +0200

    added new function to get customerId

--- backend/routes/customersRouter.js
+++ backend/routes/customersRouter.js
@@ -9,10 +9,13 @@ const {
   updating,
   deleting,
   creating,
+  getId,
 } = require("../controllers/customersController");
-
+// -- importing decodeToken middleware
+const { decodeToken } = require("../services/authentication");
 // -- setting up routes for customers
 router.route("/").get(gettingAll).post(creating);
+router.route("/info").get(decodeToken, getId);
 router.route("/:id").get(gettingById).put(updating).delete(deleting);
 // -- setting up route for getting all orders
 router.get("/:id/allorders", allOrders);
